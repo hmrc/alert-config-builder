fdb161a9783fe574800f3a46696e882c1de99545
Ali Bahman <1422984+webit4me@users.noreply.github.com>
2023-09-15 12:36:29 +0100

    TEL-3937 Add builder for Http90PercentileThreshold
    Co-authored-by: Rizina Khatun <66684341+rizinaa99@users.noreply.github.com>

--- src/test/scala/uk/gov/hmrc/alertconfig/builder/TeamAlertConfigBuilderSpec.scala
+++ src/test/scala/uk/gov/hmrc/alertconfig/builder/TeamAlertConfigBuilderSpec.scala
@@ -43,6 +43,7 @@ class TeamAlertConfigBuilderSpec extends AnyWordSpec with Matchers with BeforeAn
       alertConfigBuilder.containerKillThreshold shouldBe 1
       alertConfigBuilder.averageCPUThreshold shouldBe Int.MaxValue
       alertConfigBuilder.httpStatusThresholds shouldBe List()
+      alertConfigBuilder.http90PercentileThresholds shouldBe List()
       alertConfigBuilder.httpTrafficThresholds shouldBe List()
       alertConfigBuilder.logMessageThresholds shouldBe List()
       alertConfigBuilder.httpAbsolutePercentSplitThresholds shouldBe List()
@@ -238,6 +239,26 @@ class TeamAlertConfigBuilderSpec extends AnyWordSpec with Matchers with BeforeAn
 
       service1Config("5xx-threshold") shouldBe expected
       service2Config("5xx-threshold") shouldBe expected
+    }
+
+    "return TeamAlertConfigBuilder with correct http90PercentileThreshold" in {
+      val threshold = Http90PercentileThreshold(Some(10), Some(5))
+      val alertConfigBuilder = TeamAlertConfigBuilder.teamAlerts(Seq("service1", "service2"))
+        .withHttp90PercentileThreshold(threshold)
+
+      alertConfigBuilder.services shouldBe Seq("service1", "service2")
+      val configs = alertConfigBuilder.build.map(_.build.get.parseJson.asJsObject.fields)
+
+      configs.size shouldBe 2
+      val service1Config = configs(0)
+      val service2Config = configs(1)
+
+      val expected = JsArray(
+        JsObject("warning" -> JsNumber(10), "critical" -> JsNumber(5))
+      )
+
+      service1Config("http90PercentileThresholds") shouldBe expected
+      service2Config("http90PercentileThresholds") shouldBe expected
     }
 
     "return TeamAlertConfigBuilder with correct httpTrafficThreshold" in {
