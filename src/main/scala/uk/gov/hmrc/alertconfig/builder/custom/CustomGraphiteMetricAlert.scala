20205bf421405c0acb7d826ccbba9dc6a0aa1f5c
Muhammed Ahmed <ma3574@users.noreply.github.com>
2024-08-14 09:05:02 +0100

    TEL-4663 Additional fields for CustomHttpEndpointAlert (#175)
    Co-authored-by: Gavin Davies @gavD <gavD@users.noreply.github.com>
    Co-authored-by: Alex Tasioulis @alextcap <alextcap@users.noreply.github.com>

--- src/main/scala/uk/gov/hmrc/alertconfig/builder/custom/CustomGraphiteMetricAlert.scala
+++ src/main/scala/uk/gov/hmrc/alertconfig/builder/custom/CustomGraphiteMetricAlert.scala
@@ -41,9 +41,11 @@ import uk.gov.hmrc.alertconfig.builder.custom.TimeRangeAsMinutes.TimeRangeAsMinu
   * @param teamName
   *   All alerts are prefixed with the team name
   * @param reducerFunction
-  *   Function to use to transform multiple data points returned from query into a single value, to be compared against the specified threshold. Valid values include: COUNT, LAST, MAX, MEAN, MIN, SUM.
-  *   Note: Using the LAST reducer could result in not all data points being considered during alert evaluation, depending on the frequency at which the alert runs.
-  *   Example: An alert with a LAST reducer, that runs every 2 minutes, based on a metric that is written on a per minute basis, will only consider ~50% of the data points, potentially missing a legitimate breach of an alert threshold.
+  *   Function to use to transform multiple data points returned from query into a single value, to be compared against the specified threshold. Valid
+  *   values include: COUNT, LAST, MAX, MEAN, MIN, SUM. Note: Using the LAST reducer could result in not all data points being considered during alert
+  *   evaluation, depending on the frequency at which the alert runs. Example: An alert with a LAST reducer, that runs every 2 minutes, based on a
+  *   metric that is written on a per minute basis, will only consider ~50% of the data points, potentially missing a legitimate breach of an alert
+  *   threshold.
   * @param runbookUrl
   *   Runbook for when this alert fires
   * @param severity
@@ -54,8 +56,9 @@ import uk.gov.hmrc.alertconfig.builder.custom.TimeRangeAsMinutes.TimeRangeAsMinu
   *   Trigger point for each environment
   * @param checkIntervalMinutes
   *   Number of minutes between each check. See [[CheckIntervalMinutes]] for supported values
-  * @param queryTimeRangeMinutes 
-  *   The sample period to check data for. If you set it to FIVE_MINUTES, the alert check will evaluate data starting from 6 minutes ago until one minute ago (so that only fully shipped metrics are evaluated).
+  * @param queryTimeRangeMinutes
+  *   The sample period to check data for. If you set it to FIVE_MINUTES, the alert check will evaluate data starting from 6 minutes ago until one
+  *   minute ago (so that only fully shipped metrics are evaluated).
   */
 case class CustomGraphiteMetricAlert(
     alertName: String,
--- src/main/scala/uk/gov/hmrc/alertconfig/builder/custom/CustomHttpEndpointAlert.scala
+++ src/main/scala/uk/gov/hmrc/alertconfig/builder/custom/CustomHttpEndpointAlert.scala
@@ -18,9 +18,12 @@ package uk.gov.hmrc.alertconfig.builder.custom
 
 import uk.gov.hmrc.alertconfig.builder.custom.CustomAlertSeverity.AlertSeverity
 import com.fasterxml.jackson.annotation.JsonFilter
+import uk.gov.hmrc.alertconfig.builder.custom.CheckIntervalMinutes.CheckIntervalMinutes
+import uk.gov.hmrc.alertconfig.builder.custom.TimeRangeAsMinutes.TimeRangeAsMinutes
 
 /** This alert will notify when it fails to receive the expected response (status code and/or string pattern) from the given endpoint
-  *
+  * @param checkIntervalMinutes
+  *   Number of minutes between each check. See [[CheckIntervalMinutes]] for supported values
   * @param checkName
   *   Name to be used in the metric path. Only lowercase letters and - are allowed: `^[a-z-]+$`
   * @param componentName
@@ -37,13 +40,23 @@ import com.fasterxml.jackson.annotation.JsonFilter
   *   The HTTP endpoint to be checked.
   * @param integrations
   *   Which PagerDuty integrations to direct this alert to
+  * @param pendingPeriodMinutes
+  *   Amount of time in minutes that a threshold needs to be breached before the alert fires
+  * @param queryTimeRangeMinutes
+  *   The sample period to check data for. If you set it to FIVE_MINUTES, the alert check will evaluate data starting from 6 minutes ago until one
+  *   minute ago (so that only fully shipped metrics are evaluated).
   * @param runbookUrl
   *   Runbook for when this alert fires
   * @param severity
   *   The severity level of the alert (critical or warning). Defaults to critical.
+  * @param summary
+  *   The description to populate in PagerDuty when the alert fires
+  * @param teamName
+  *   All alerts are prefixed with the team name
   */
 @JsonFilter("RemoveEnvironmentsEnabledField")
 case class CustomHttpEndpointAlert(
+    checkIntervalMinutes: Option[CheckIntervalMinutes] = None,
     checkName: String,
     componentName: String,
     cronCheckSchedule: String,
@@ -52,6 +65,10 @@ case class CustomHttpEndpointAlert(
     expectedStringInResponse: Option[String] = None,
     httpEndpoint: String,
     integrations: Seq[String],
+    pendingPeriodMinutes: Option[Int] = None,
+    queryTimeRangeMinutes: TimeRangeAsMinutes = TimeRangeAsMinutes.ONE_HOUR,
     runbookUrl: Option[String],
-    severity: AlertSeverity
+    severity: AlertSeverity,
+    summary: String,
+    teamName: String
 ) extends CustomAlert
