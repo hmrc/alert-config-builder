6638eceda0f6954b8a615ad60bd15d978d2c7422
Stephen Palfreyman <18111914+sjpalf@users.noreply.github.com>
2024-09-09 10:10:54 +0100

    TEL-4805 allow generating multiple alerts (per instance) from graphite checks (#189)
    Co-authored-by: Muhammed Ahmed <ma3574@users.noreply.github.com>

--- src/main/scala/uk/gov/hmrc/alertconfig/builder/custom/CustomGraphiteMetricAlert.scala
+++ src/main/scala/uk/gov/hmrc/alertconfig/builder/custom/CustomGraphiteMetricAlert.scala
@@ -26,20 +26,25 @@ import uk.gov.hmrc.alertconfig.builder.custom.TimeRangeAsMinutes.TimeRangeAsMinu
   *
   * @param alertName
   *   Name that the alert will be created with
-  * @param pendingPeriodMinutes
-  *   Amount of time in minutes that a threshold needs to be breached before the alert fires
-  * @param dashboardUri
-  *   Grafana uri to link to. This should just be the uri path and not include the domain
+  * @param alertPerInstance
+  *   Whether to generate multiple alerts if the graphite metric query produces multiple result instances
+  * @param checkIntervalMinutes
+  *   Number of minutes between each check. See [[CheckIntervalMinutes]] for supported values
   * @param dashboardPanelId
   *   Specific panel to deep link to that is specific to this alert
+  * @param dashboardUri
+  *   Grafana uri to link to. This should just be the uri path and not include the domain
   * @param integrations
   *   Which PagerDuty integrations to direct this alert to
   * @param operator
   *   Whether to evaluate the metric as greater than or less than
+  * @param pendingPeriodMinutes
+  *   Amount of time in minutes that a threshold needs to be breached before the alert fires
   * @param query
   *   Graphite query you're running
-  * @param teamName
-  *   All alerts are prefixed with the team name
+  * @param queryTimeRangeMinutes
+  *   The sample period to check data for. If you set it to FIVE_MINUTES, the alert check will evaluate data starting from 6 minutes ago until one
+  *   minute ago (so that only fully shipped metrics are evaluated).
   * @param reducerFunction
   *   Function to use to transform multiple data points returned from query into a single value, to be compared against the specified threshold. Valid
   *   values include: COUNT, LAST, MAX, MEAN, MIN, SUM. Note: Using the LAST reducer could result in not all data points being considered during alert
@@ -52,28 +57,26 @@ import uk.gov.hmrc.alertconfig.builder.custom.TimeRangeAsMinutes.TimeRangeAsMinu
   *   The severity of this alert. E.g. Warning or Critical
   * @param summary
   *   The description to populate in PagerDuty when the alert fires
+  * @param teamName
+  *   All alerts are prefixed with the team name
   * @param thresholds
   *   Trigger point for each environment
-  * @param checkIntervalMinutes
-  *   Number of minutes between each check. See [[CheckIntervalMinutes]] for supported values
-  * @param queryTimeRangeMinutes
-  *   The sample period to check data for. If you set it to FIVE_MINUTES, the alert check will evaluate data starting from 6 minutes ago until one
-  *   minute ago (so that only fully shipped metrics are evaluated).
   */
 case class CustomGraphiteMetricAlert(
     alertName: String,
+    alertPerInstance: Boolean = false,
     checkIntervalMinutes: Option[CheckIntervalMinutes] = None,
-    pendingPeriodMinutes: Option[Int] = None,
-    dashboardUri: Option[String],
     dashboardPanelId: Option[Int],
+    dashboardUri: Option[String],
     integrations: Seq[String],
     operator: EvaluationOperator,
+    pendingPeriodMinutes: Option[Int] = None,
     query: String,
-    teamName: String,
+    queryTimeRangeMinutes: TimeRangeAsMinutes = TimeRangeAsMinutes.FIFTEEN_MINUTES,
     reducerFunction: ReducerFunction,
     runbookUrl: Option[String],
     severity: AlertSeverity,
     summary: String,
-    thresholds: EnvironmentThresholds,
-    queryTimeRangeMinutes: TimeRangeAsMinutes = TimeRangeAsMinutes.FIFTEEN_MINUTES
+    teamName: String,
+    thresholds: EnvironmentThresholds
 ) extends CustomAlert
