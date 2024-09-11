4d6dc6585392323932dbd588a76750f326772ee1
Stephen Palfreyman <18111914+sjpalf@users.noreply.github.com>
2024-09-11 18:05:11 +0100

    TEL-4801 add custom alert type based on logs as a percentage (#191)
    * TEL-4801 add new custom alert type CustomElasticsearchPercentageAlert
    
    Co-authored-by: Muhammed Ahmed <ma3574@users.noreply.github.com>

--- src/main/scala/uk/gov/hmrc/alertconfig/builder/custom/CustomAlertConfigYamlBuilder.scala
+++ src/main/scala/uk/gov/hmrc/alertconfig/builder/custom/CustomAlertConfigYamlBuilder.scala
@@ -25,6 +25,7 @@ case class CustomAlertsTopLevel(alerts: CustomAlerts)
 
 case class CustomAlerts(
     customElasticsearchAlerts: Seq[CustomElasticsearchAlert],
+    customElasticsearchPercentageAlerts: Seq[CustomElasticsearchPercentageAlert],
     customGraphiteMetricAlerts: Seq[CustomGraphiteMetricAlert],
     customCloudWatchMetricAlerts: Seq[CustomCloudWatchMetricAlert]
 )
@@ -41,6 +42,11 @@ object CustomAlertConfigYamlBuilder {
         alert.copy(thresholds = alert.thresholds.removeAllOtherEnvironmentThresholds(currentEnvironment))
       }
 
+    val customElasticsearchPercentageAlerts: Seq[CustomElasticsearchPercentageAlert] = activeAlerts
+      .collect { case alert: CustomElasticsearchPercentageAlert =>
+        alert.copy(thresholds = alert.thresholds.removeAllOtherEnvironmentThresholds(currentEnvironment))
+      }
+
     val customGraphiteMetricAlerts: Seq[CustomGraphiteMetricAlert] = activeAlerts
       .collect { case alert: CustomGraphiteMetricAlert =>
         alert.copy(thresholds = alert.thresholds.removeAllOtherEnvironmentThresholds(currentEnvironment))
@@ -51,7 +57,8 @@ object CustomAlertConfigYamlBuilder {
         alert.copy(thresholds = alert.thresholds.removeAllOtherEnvironmentThresholds(currentEnvironment))
       }
 
-    val separatedAlerts = CustomAlertsTopLevel(CustomAlerts(customElasticsearchAlerts, customGraphiteMetricAlerts, customCloudWatchMetricAlerts))
+    val separatedAlerts = CustomAlertsTopLevel(
+      CustomAlerts(customElasticsearchAlerts, customElasticsearchPercentageAlerts, customGraphiteMetricAlerts, customCloudWatchMetricAlerts))
 
     mapper.writeValue(saveLocation, separatedAlerts)
   }
@@ -87,9 +94,10 @@ object CustomAlertConfigYamlBuilder {
     */
   private def isAlertDefinedForEnv(alert: CustomAlert, currentEnvironment: Environment): Boolean = {
     alert match {
-      case alert: CustomGraphiteMetricAlert   => alert.thresholds.isEnvironmentDefined(currentEnvironment)
-      case alert: CustomElasticsearchAlert    => alert.thresholds.isEnvironmentDefined(currentEnvironment)
-      case alert: CustomCloudWatchMetricAlert => alert.thresholds.isEnvironmentDefined(currentEnvironment)
+      case alert: CustomCloudWatchMetricAlert        => alert.thresholds.isEnvironmentDefined(currentEnvironment)
+      case alert: CustomElasticsearchAlert           => alert.thresholds.isEnvironmentDefined(currentEnvironment)
+      case alert: CustomElasticsearchPercentageAlert => alert.thresholds.isEnvironmentDefined(currentEnvironment)
+      case alert: CustomGraphiteMetricAlert          => alert.thresholds.isEnvironmentDefined(currentEnvironment)
       case other => throw new IllegalArgumentException(s"isAlertDefinedForEnv is not defined for ${other.getClass.getSimpleName}. Please update it.")
     }
   }
@@ -120,10 +128,11 @@ object CustomAlertConfigYamlBuilder {
     */
   private def updateIntegrationsForCustomAlert(customAlert: CustomAlert, enabledIntegrations: Seq[String]): CustomAlert = {
     customAlert match {
-      case alert: CustomCloudWatchMetricAlert => alert.copy(integrations = enabledIntegrations)
-      case alert: CustomElasticsearchAlert    => alert.copy(integrations = enabledIntegrations)
-      case alert: CustomGraphiteMetricAlert   => alert.copy(integrations = enabledIntegrations)
-      case _                                  => throw new IllegalArgumentException(s"Unsupported CustomAlert type: ${customAlert.getClass.getName}")
+      case alert: CustomCloudWatchMetricAlert        => alert.copy(integrations = enabledIntegrations)
+      case alert: CustomElasticsearchAlert           => alert.copy(integrations = enabledIntegrations)
+      case alert: CustomElasticsearchPercentageAlert => alert.copy(integrations = enabledIntegrations)
+      case alert: CustomGraphiteMetricAlert          => alert.copy(integrations = enabledIntegrations)
+      case other => throw new IllegalArgumentException(s"Unsupported CustomAlert type: ${customAlert.getClass.getName}")
     }
   }
 
--- src/main/scala/uk/gov/hmrc/alertconfig/builder/custom/CustomElasticsearchPercentageAlert.scala
+++ src/main/scala/uk/gov/hmrc/alertconfig/builder/custom/CustomElasticsearchPercentageAlert.scala
@@ -19,33 +19,31 @@ package uk.gov.hmrc.alertconfig.builder.custom
 import uk.gov.hmrc.alertconfig.builder.custom.CheckIntervalMinutes.CheckIntervalMinutes
 import uk.gov.hmrc.alertconfig.builder.custom.CustomAlertSeverity.AlertSeverity
 import uk.gov.hmrc.alertconfig.builder.custom.EvaluationOperator.EvaluationOperator
-import uk.gov.hmrc.alertconfig.builder.custom.ReducerFunction.ReducerFunction
 import uk.gov.hmrc.alertconfig.builder.custom.TimeRangeAsMinutes.TimeRangeAsMinutes
 
-/** Elasticsearch logs based alert.
+/** Custom ElasticSearch alert based on the percentage of a subset of records.
   *
   * @param alertName
   *   Name that the alert will be created with
   * @param checkIntervalMinutes
   *   Number of minutes between each check. See [[CheckIntervalMinutes]] for supported values
-  * @param pendingPeriodMinutes
-  *   Amount of time in minutes that a threshold needs to be breached before the alert fires
-  * @param kibanaDashboardUri
-  *   Kibana uri to link to. This should just be the uri path and not include the domain
   * @param integrations
   *   Which PagerDuty integrations to direct this alert to n
-  * @param luceneQuery
-  *   Query to make to Elasticsearch
+  * @param kibanaDashboardUri
+  *   (Optional) Kibana uri to link to. This should just be the uri path and not include the domain
+  * @param luceneQuerySubset
+  *   Query to make to Elasticsearch to get subset of records (to calculate as percentage of luceneQueryTotal)
+  * @param luceneQueryTotal
+  *   Query to make to Elasticsearch to get total records (to calculate percentage from)
   * @param operator
   *   Whether to evaluate the metric as greater than or less than
-  * @param reducerFunction
-  *   Function to use to transform multiple data points returned from query into a single value, to be compared against the specified threshold. Valid
-  *   values include: COUNT, LAST, MAX, MEAN, MIN, SUM. Note: Using the LAST reducer could result in not all data points being considered during alert
-  *   evaluation, depending on the frequency at which the alert runs. Example: An alert with a LAST reducer, that runs every 2 minutes, based on a
-  *   metric that is written on a per minute basis, will only consider ~50% of the data points, potentially missing a legitimate breach of an alert
-  *   threshold.
+  * @param pendingPeriodMinutes
+  *   Amount of time in minutes that a threshold needs to be breached before the alert fires. Defaults to fire immediately
+  * @param queryTimeRangeMinutes
+  *   The sample period to check data for. If you set it to FIVE_MINUTES, the alert check will evaluate data starting from 6 minutes ago until one
+  *   minute ago (so that only fully shipped metrics are evaluated).
   * @param runbookUrl
-  *   Runbook for when this alert fires
+  *   (Optional) Runbook for when this alert fires
   * @param severity
   *   The severity of this alert. E.g. Warning or Critical
   * @param summary
@@ -54,23 +52,20 @@ import uk.gov.hmrc.alertconfig.builder.custom.TimeRangeAsMinutes.TimeRangeAsMinu
   *   All alerts are prefixed with the team name
   * @param thresholds
   *   Trigger point for each environment
-  * @param queryTimeRangeMinutes
-  *   The sample period to check data for. If you set it to FIVE_MINUTES, the alert check will evaluate data starting from 6 minutes ago until one
-  *   minute ago (so that only fully shipped metrics are evaluated).
   */
-case class CustomElasticsearchAlert(
+case class CustomElasticsearchPercentageAlert(
     alertName: String,
     checkIntervalMinutes: Option[CheckIntervalMinutes] = None,
-    pendingPeriodMinutes: Option[Int] = None,
-    kibanaDashboardUri: Option[String] = None,
     integrations: Seq[String],
-    luceneQuery: String,
-    operator: EvaluationOperator,
-    reducerFunction: ReducerFunction,
+    kibanaDashboardUri: Option[String] = None,
+    luceneQuerySubset: String,
+    luceneQueryTotal: String,
+    operator: EvaluationOperator = EvaluationOperator.GREATER_THAN,
+    pendingPeriodMinutes: Option[Int] = None,
+    queryTimeRangeMinutes: TimeRangeAsMinutes = TimeRangeAsMinutes.FIFTEEN_MINUTES,
     runbookUrl: Option[String] = None,
     severity: AlertSeverity,
     summary: String,
     teamName: String,
-    thresholds: EnvironmentThresholds,
-    queryTimeRangeMinutes: TimeRangeAsMinutes = TimeRangeAsMinutes.FIFTEEN_MINUTES
+    thresholds: EnvironmentThresholds
 ) extends CustomAlert
